da7e9238bf9be3d74e9030eb5fbc1cdb4a78820d
kabik <k2.s1k.x20@gmail.com>
2019-08-02 00:44:53 +0900

    FNV-1 64bit

--- hash_algorithms/fnv1.py
+++ hash_algorithms/fnv1.py
@@ -1,4 +1,12 @@
 import struct
 
+FNV_OFFSET_BASIS_64 = 14695981039346656037
+FNV_PRIME_64        = 1099511628211
+FNV_BITMASK_64      = 18446744073709551615
+
 def hash_exec(str):
-    return str
+    hash = FNV_OFFSET_BASIS_64
+    for b in str.encode():
+        hash = (FNV_PRIME_64 * hash) ^ b
+        hash = hash & FNV_BITMASK_64
+    return hash
